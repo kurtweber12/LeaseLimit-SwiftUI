26f6b8354022f5953081526b97e40ff04036432f
kurtweber12 <kkw1995@hotmail.com>
2023-12-22 17:55:12 -0500

    Initial Commit

---
+++ LeaseLimit/LeaseLimitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaseLimitApp.swift
+//  LeaseLimit
+//
+//  Created by Kurt Weber on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LeaseLimitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
